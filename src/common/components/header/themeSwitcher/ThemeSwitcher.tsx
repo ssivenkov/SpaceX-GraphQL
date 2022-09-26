748b21c1d1c09665dd75c3828bbaf412c0a39bf6
ssivenkov <ser.sivenkov@gmail.com>
2022-09-26 10:48:05 +0300

    fix dark mode request

--- src/common/components/header/themeSwitcher/ThemeSwitcher.tsx
+++ src/common/components/header/themeSwitcher/ThemeSwitcher.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { useQuery } from '@apollo/client';
-import { darkModeQuery } from 'apollo/querys/darkModeQuery';
+import { useReactiveVar } from '@apollo/client';
+import { isDarkModeVar } from 'apollo/reactiveVars/darkModeVars';
 import { changeTheme } from 'App';
 import { darkThemeColors, lightThemeColors } from 'common/colors/themeColors';
 import { DARK_THEME, LIGHT_THEME } from 'common/constants/constants';
@@ -9,14 +9,14 @@ import { DARK_THEME, LIGHT_THEME } from 'common/constants/constants';
 import styles from './style.module.scss';
 
 export const ThemeSwitcher = () => {
-  const { data } = useQuery(darkModeQuery);
+  const isDarkMode = useReactiveVar<boolean>(isDarkModeVar);
 
   const userTheme = localStorage.getItem('userTheme') ?? false;
 
   const isDarkTheme = userTheme === DARK_THEME;
 
   const setTheme = () => {
-    if (data?.isDarkMode) {
+    if (isDarkMode) {
       changeTheme({ userTheme: LIGHT_THEME, themeColors: lightThemeColors });
     } else changeTheme({ userTheme: DARK_THEME, themeColors: darkThemeColors });
   };
